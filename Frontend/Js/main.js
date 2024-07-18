67f0abb4670aaaf11c569a43336a8209bc0f4926
ChrisPau0530 <230809@utxicotepec.edu.mx>
2024-07-17 22:50:03 -0600

    Se agrega la carpeta Js en su carpeta de Fronend
    Se agrega ejemplo de js y un readme

---
+++ Frontend/Js/main.js
@@ -0,0 +1,79 @@
+//tipos de datos en JS
+// String
+// Boolean
+// Number
+// null
+// undefined
+// Symbol
+// NaN (not at number)
+
+// nombre = 'Michelle';
+// caja = 'ropa';
+// numero1 = 6;
+// foco = true;
+// universidad = 10;
+
+// console.log(nombre);
+// console.log(caja);
+// console.log(numero1);
+// console.log(foco);
+
+// alert(universidad);
+
+// nombre = "mich";
+// a=5;
+// b=2;
+
+// console.log(nombre);
+// document.write(nombre);
+// alert(nombre);
+ 
+//Variables y constantes
+
+// var nombre = "michelle";
+// let edad = 18;
+// const alumna = "Victoria";
+
+// let numero1, numero2, resultado;
+// numero1 = parseInt(prompt("Introduce un número"));
+// numero2 = parseInt(prompt("Introduce un segundo número"));
+// resultado = numero1+numero2;
+// alert(resultado);
+
+// var soyEstudiante = "Teresa";
+
+// function universidad(){
+//     var soyEstudiante = "Michelleeee";
+//     console.log(soyEstudiante);
+// }
+
+// universidad();
+// console.log(soyEstudiante);
+
+//ejemplo de variable global
+// var caja = "esto está afuera del bloque";
+
+// if(true){
+//     var caja = "esto está dentro de la caja";
+//     console.log(caja)
+// }
+
+// console.log(caja);
+
+//objetos
+
+const car ={type: "fiat", model: "500", color:"white"}
+//document.getElementById("demo").innerHTML="El tipo de coche es "+car.type;
+// document.getElementById("type").innerHTML="El tipo de coche es "+car.type;
+// document.getElementById("modelo").innerHTML="El modelo de coche es "+car.model;
+// document.getElementById("color").innerHTML="El color de coche es "+car.color;
+
+
+ numero1= parseInt(document.getElementById("numero1").innerHTML);
+ numero2=parseInt(document.getElementById("numero2").innerHTML);
+    resultado=numero1+numero2;
+ document.getElementById("resultado").innerHTML="El resultado es "+resultado;
+
+
+
+
---
+++ Frontend/Js/readme.md
@@ -0,0 +1,28 @@
+# JAVASCRIPT ![MYSQL ](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
+
+Este archivo contiene el código necesario para ver sus tipos de datos que podemos colocar en un ejemplo o en un programa que lleve relacionado Javascript
+
+- **`Parseint`**: Especifica que el tipo de dato sera entero.
+- **`Parsefloat`**: Define que el tipo de dato sera flotante.
+- **`getElementById`**: Se trata cuando una etiqueta tiene un ID principal
+
+Lo que pasa en este programa es una explicacion breve sobre los tipos de datos de   `JAVASCRIPT`
+
+
+## Estructura de Archivos
+
+>IntegradoraI-NombreProyecto<br>
+>| - Backend <br>
+>&nbsp;&nbsp;|- DB_Connection<br>
+>&nbsp;&nbsp;|- PHP<br>
+>| - Database<br>
+> | - **FrontEnd** <br>
+>| - Documentation<br>
+
+
+## Equipo de Desarrollo
+
+|Integrante|Contacto|Rol|Observaciones|
+|------------|--------|---|---|
+|M.T.I. Marco Antnio Ramírez Hernández|[@MTI-MarcoRH](https://github.com/MTI-MarcoRH)|Líder del  Desarrollo Frontend |😔 No ha revisado|
+|Christian Paul Rodriguez Perez|[@ChrisPau0530](https://github.com/ChrisPau0530)|Desarrollador Frontend|✅ Revisado y aprobado| 
